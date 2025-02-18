5f5a5087ed5acbce3e5965853a696be02731d5b6
harridotcom <shreekantpukale25@gmail.com>
2025-02-19 01:33:36 +0530

    mvvm and repo files creation

---
+++ app/src/main/java/com/medicare/mvvm/CareViewModel.kt
@@ -0,0 +1,7 @@
+package com.medicare.mvvm
+
+import androidx.lifecycle.ViewModel
+
+class CareViewModel: ViewModel() {
+
+}
---
+++ app/src/main/java/com/medicare/repository/CareRepository.kt
@@ -0,0 +1,4 @@
+package com.medicare.repository
+
+class CareRepository {
+}
